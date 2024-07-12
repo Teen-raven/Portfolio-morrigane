5d10439538857badd8ead797fd3f1afa77f4e992
Teen-raven <bakatchii@gmail.com>
2024-07-12 18:05:18 +0200

    react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Portfolio-morrigane">
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
